ccff9dc1d640de746473ede57338fefceb4d1278
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-09-06 20:27:09 +0530

    Adding Service to the Highest Possible Level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { AccountService } from './accounts.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [AccountService],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   acountsList: { name: string; status: string }[] = [];
 
+  //Injecting Service through dependency Injection
   constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
